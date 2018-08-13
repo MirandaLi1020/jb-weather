7eb3936de80668ef01e582cec3d59ea445579b65
Yunzhi Hou <stewarthou@hotmail.com>
2018-08-13 15:52:03 +1000

    Added integration tests for auto test during development

---
+++ integration-tests/server.test.js
@@ -0,0 +1,28 @@
+/* globals describe, it, after */
+
+const request = require('supertest')
+const mongoose = require('mongoose')
+
+const server = require('../server')
+
+describe('server.js tests', () => {
+  // Just a simple integration test for testing in development environment
+  it('should response 200 for GET /weather/australia/melbourne with valid API key', () => {
+    return request(server)
+      .get('/weather/australia/melbourne')
+      .auth('27f01d68e204427fbeb7c0dff06c7e94', 'x')
+      .expect(200)
+  })
+  it('should response 401 for GET /weather/australia/melbourne with invalid API key', () => {
+    return request(server)
+      .get('/weather/australia/melbourne')
+      .auth('INVALID_API_KEY', 'x')
+      .expect(401)
+  })
+
+  after(async () => {
+    if (mongoose.connection.readyState) {
+      await mongoose.disconnect()
+    }
+  })
+})
